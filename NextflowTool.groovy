6aeaf9275154323861a29861b6476a42bdf4cf07
cc52 <cc52@sanger.ac.uk>
2025-10-07 16:32:38 +0100

    remove list dash symbol to avoid visual clash with param doubledash

--- NextflowTool.groovy
+++ NextflowTool.groovy
@@ -254,7 +254,7 @@ class NextflowTool {
             log.info "${colors.purple} The following parameters were provided on the command line: ${colors.reset}" 
             log.info indent
             commandLineParams.each { key, value ->
-                log.info indent + "- ${key}: ${value}"
+                log.info indent + "${key}: ${value}"
                 }
             }
         log.info indent //whitespace after params
